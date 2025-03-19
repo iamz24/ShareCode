ead737c0d3e32a627baca554dd3e7ebfe285c0df
Nguyễn Việt Dũng <97619614+iamz24@users.noreply.github.com>
2025-03-19 21:48:46 +0700

    Add files via upload

---
+++ Alert_FileCheck/Alert_FileCheck/cdkn/src/main/java/alert/checkfile/malicious/cdkn/controller/FileCheckController.java
@@ -0,0 +1,33 @@
+package alert.checkfile.malicious.cdkn.controller;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import alert.checkfile.malicious.cdkn.service.FileCheckService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+public class FileCheckController {
+    @Async
+    @PostMapping("/Check-File")
+    public void checkFile(@RequestBody String json) throws Exception {
+        //TODO: process POST request
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(json);
+        JsonNode resultNode = rootNode.path("result");
+        JsonNode hash = resultNode.path("hash");
+        JsonNode path = resultNode.path("path");
+        JsonNode user = resultNode.path("user");
+
+        FileCheckService check = new FileCheckService();
+        check.checkFileByVirusTotal(hash.asText(), path.asText(), user.asText());
+        
+    }
+    
+}
